dc737d41b9c74ce9d0acb1623f64b19cf2c98424
GLCVLL <g.ioele96@outlook.it>
2023-06-20 18:14:40 +0200

    fixed error in deletoTodo, now delete items by id

--- js/script.js
+++ js/script.js
@@ -7,19 +7,22 @@ const app = Vue.createApp({
     data() {
         return {
             todos: [
-                { text: 'Comprare shampoo', done: false },
-                { text: 'Pulire giardino', done: false },
-                { text: 'Fare passeggiata con il cane', done: false },
-                { text: 'Preparare torta compleanno', done: true },
-                { text: 'Sistemare cucina', done: false }
+                { id: 1, text: 'Comprare shampoo', done: false },
+                { id: 2, text: 'Pulire giardino', done: false },
+                { id: 3, text: 'Fare passeggiata con il cane', done: false },
+                { id: 4, text: 'Preparare torta compleanno', done: true },
+                { id: 5, text: 'Sistemare cucina', done: false }
             ],
 
             newTodoText: '',
         };
     },
     methods: {
-        deleteTodo(index) {
-            this.todos.splice(index, 1);
+        deleteTodo(id) {
+            const index = this.todos.findIndex(todo => todo.id === id);
+            if (index !== -1) {
+              this.todos.splice(index, 1);
+            }
         },
 
         addTodo() {
