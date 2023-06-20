6b72cedbfaefe044678acb921d10cc601405f852
GLCVLL <g.ioele96@outlook.it>
2023-06-20 17:27:37 +0200

    added toDoList and object array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 console.log('js ok');
 
 console.log('vue ok', Vue);
+
+const app = Vue.createApp({
+    name: 'ToDoList',
+    data() {
+        return {
+            todos: [
+                { text: 'Comprare shampoo', done: false },
+                { text: 'Pulire giardino', done: true },
+                { text: 'Fare passeggiata con il cane', done: false },
+                { text: 'Preparare torta compleanno', done: true },
+                { text: 'Sistemare cucina', done: false }
+            ]
+        }
+    }
+});
+
+app.mount('#root');
